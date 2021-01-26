b34eb34a257e2fe54a76dce58a69317f9861b192
James Clarke <james@bitfudge.com>
2021-01-26 11:30:50 +0000

    added ip address discovery script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # scripts
+
+Some random scripts that I wrote to do some cloud related stuff.
+
+All really simple, but kinda useful.
+
+## getPublicIPs.ps1
+
+Extracts all of the public IP addresses under an Azure tenant.
---
+++ getPublicIPs.ps1
@@ -0,0 +1,30 @@
+﻿$sublist = Get-AzSubscription
+
+$iplist = @()
+$prefixlist = @()
+$counter = 0
+foreach ($sub in $sublist)
+{
+    $counter++
+    $status = "Collecting IP for Subscription $counter of " + $sublist.count
+    Write-Progress -Activity $status -CurrentOperation $sub.name -PercentComplete (($counter / $sublist.count) * 100)
+    
+    Select-AzSubscription -Subscription $sub.Id
+    
+    $subips = Get-AzPublicIpAddress
+    $subips | Add-Member -NotePropertyName SubscriptionId -NotePropertyValue $sub.SubscriptionId
+    $subips | Add-Member -NotePropertyName SubscriptionName -NotePropertyValue $sub.Name
+    $iplist += $subips.where({$_.IpAddress -ne 'Not Assigned'})
+}
+
+foreach ($ip in $iplist)
+{
+    $url = "https://" + $ip.IpAddress
+    $request = Invoke-WebRequest $url
+
+    if ($request.StatusCode -eq 200){
+        print $request.headers
+    }
+}
+
+$iplist | Select-Object -Property SubscriptionName,Name,ResourceGroupName,PublicIpAllocationMethod,IPAddress
